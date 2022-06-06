9f94c324c6decc4f650972c47cc18d20f5f7d131
Peter Vereshagin <peter@vereshagin.org>
2022-06-06 09:41:18 +0000

    test: :art: improve run and exit

--- index.test.js
+++ index.test.js
@@ -4,8 +4,9 @@ const { spawn } = require('child_process')
 
 test(`Run index and exit`, async () => {
   try {
-    const cp = spawn(`npm`, `run start -- --username=your_username`.split(/\s+/))
-    expect(cp).toBeTruthy()
+    let cp
+    expect(() => { cp = spawn(`npm`, `run start -- --username=your_username`.split(/\s+/)) }).not.ToThrow()
+    expect(cp).toBeInstanceOf(ChildProcessWithoutNullStreams)
     const exitCode = await new Promise(resolve => { cp.on(`close`, exitCode => resolve(exitCode)) })
     expect(exitCode).toBe(0)
   } catch (e) {
