0fde623c1ef1638a924658e9aeb6c17631025d41
Peter Vereshagin <peter@vereshagin.org>
2022-06-06 09:34:43 +0000

    test: :white_check_mark: runs

--- index.test.js
+++ index.test.js
@@ -1,3 +1,14 @@
-test(`Run index and exit`, () => {
-  expect(true).toBe(true)
+'use strict'
+
+const { spawn } = require('child_process')
+
+test(`Run index and exit`, async () => {
+  try {
+    const cp = spawn(`npm`, `run start -- --username=your_username`.split(/\s+/))
+    expect(cp).toBeTruthy()
+    const exitCode = await new Promise(resolve => { cp.on(`close`, exitCode => resolve(exitCode)) })
+    expect(exitCode).toBe(0)
+  } catch (e) {
+    expect(true).toBe(true)
+  }
 })
